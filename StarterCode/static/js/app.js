818ba2fab9e2e3cb4c0e1b26d6cf76e39776c300
emedina2 <74868338+emedina2@users.noreply.github.com>
2021-04-13 21:46:22 -0500

    Update app.js

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,10 +1,10 @@
-//set up variables for plot locations on page
+//set up ables for plot locations on page
 var barChart = d3.select("#bar");
 var gaugePlot = d3.select("#gauge");
 var bubblePlot = d3.select("#bubble");
 var dropdown = d3.select("#sample-metadata");
-var sample = dropdown.node().value
-
+//var sample = dropdown.node().value
+var sample = "940"
 //add unpack function
 function unpack(rows, index) {
     return rows.map(function(row) {
@@ -12,20 +12,26 @@ function unpack(rows, index) {
     });
     }
 //create function & variables for data
-
+var names;
+var metadata;
+var samples;
 function getTableData(sample){
 d3.json("./data/samples.json")
 .then(function(data) {
-     var names = data.names;
-     var metadata = data.metadata;
-     var samples = data.samples;
-     console.log(names)
-     console.log(metadata)
-     console.log(samples)
+     names = data.names;
+     metadata = data.metadata;
+     samples = data.samples;
+    //  console.log(names)
+    //  console.log(metadata)
+    //  console.log(samples)
+     if (names.indexOf(sample) !== -1){
+         console.log(samples[sample.index]);
+         names = 
+     }
 })
 }
 
-getTableData(940)
+getTableData(sample)
 
     
 
