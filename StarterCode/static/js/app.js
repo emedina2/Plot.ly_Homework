bf5902acdc949b7bd2e31af9b57beb1efe2b1ce8
emedina2 <74868338+emedina2@users.noreply.github.com>
2021-04-13 19:27:59 -0500

    Started js code

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,5 @@
+//set up variables for plot locations on page
+var barChart = d3.select("#bar");
+var gaugePlot = d3.select("#gauge");
+var bubblePlot = d3.select("#bubble");
+var dropdown = d3.select("#sample-metadata");
