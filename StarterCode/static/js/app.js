ebedfa08daf26fecad7d8e0e63ddcbf827b1c212
emedina2 <74868338+emedina2@users.noreply.github.com>
2021-04-13 21:23:28 -0500

    Added unpack

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -3,3 +3,30 @@ var barChart = d3.select("#bar");
 var gaugePlot = d3.select("#gauge");
 var bubblePlot = d3.select("#bubble");
 var dropdown = d3.select("#sample-metadata");
+var sample = dropdown.node().value
+
+//add unpack function
+function unpack(rows, index) {
+    return rows.map(function(row) {
+    return row[index];
+    });
+    }
+//create function & variables for data
+
+function getTableData(sample){
+d3.json("./data/samples.json")
+.then(function(data) {
+     var names = data.names;
+     var metadata = data.metadata;
+     var samples = data.samples;
+     console.log(names)
+     console.log(metadata)
+     console.log(samples)
+})
+}
+
+getTableData(940)
+
+    
+
+
