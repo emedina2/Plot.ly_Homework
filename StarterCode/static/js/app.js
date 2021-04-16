80737bbfa3c149e40108fff1429d16436e775195
emedina2 <74868338+emedina2@users.noreply.github.com>
2021-04-15 19:35:20 -0500

    Added values to dropdown

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -3,6 +3,8 @@ var barChart = d3.select("#bar");
 var gaugePlot = d3.select("#gauge");
 var bubblePlot = d3.select("#bubble");
 var dropdown = d3.select("select");
+var sampleMetadata = d3.select("#sample-metadata");
+var selection;
 
 //var sample = dropdown.node().value
 var data = d3.json("./data/samples.json")
@@ -14,13 +16,33 @@ function unpack(rows, index) {
     });
     }
 
+//Detect dropdown change
+dropdown.on("onchange",function(event){
+    console.log(event)
+} )
+
+
+//append data to metadata box
+function filterData(metadata) {
+    metadata.id === sample
+}
+function addMetadata([key, value]) {
+    sampleMetadata.append("p")
+    .text(`${key} : ${value}`)
+}
+
 //initialize page function
 function init() {
 data.then(function(nameDropdown) {
     Object.entries(nameDropdown.names).forEach(function([key, value]) {
     var options = dropdown.append("option")
+    .attr('value', value)
     options.text(value)
     })
+    Object.entries(nameDropdown.metadata)
+    .filter(filterData)
+    .forEach(addMetadata)
+    console.log(`The selected Subject is ${selection}`)  
     
 })
 };
